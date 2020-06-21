de9fdfae12a1e8461b1776d67c23377f36697086
Alexandru Cr <45730710+cralex11@users.noreply.github.com>
2020-06-21 17:25:17 +0300

    local storage
    add local storage
    fix it to work
    add option to register new book
    show books from local storage

--- script.js
+++ script.js
@@ -9,11 +9,6 @@ const isRead = document.querySelector('.read')
 const addBookNav = document.getElementById('addBookNav')
 const addBookSection = document.getElementsByClassName('add-book')
 const addBookBtn = document.getElementById('add-book-btn')
-// add book window
-const bookNameAddNewBookSection = document.getElementById('bookName').value
-const bookAuthorAddNewBookSection = document.getElementById('bookAuthor').value
-const bookPagesAddNewBookSection = document.getElementById('pageNumber').value
-const bookIsReadAddNewBookSection = document.getElementById('isReadCheckbox')
 
 const book = [
 	{
@@ -61,6 +56,14 @@ const book = [
 // 	bookBlock.after(cloneBlock)
 // }
 
+// OPEN ADD BOOK SECTION
+addBookNav.addEventListener('click', () => {
+	if (addBookSection[0].style.display == 'block')
+		addBookSection[0].style.display = 'none'
+	else addBookSection[0].style.display = 'block'
+})
+// TODO: to add focus/blur
+// ---------FUNCTIONS-----
 // CHECK IF THE BOOK IS READ AND CHANGE ICON AND VALUE
 function haveRead(bookButt) {
 	bookButt = bookButt.children[0]
@@ -73,6 +76,7 @@ function haveRead(bookButt) {
 		bookButt.removeAttribute('name')
 		bookButt.setAttribute('name', 'book-outline')
 	}
+	// TODO: fix haveRead function
 	book.forEach((element) => {
 		if (element.title === title) {
 			element.isRead = !element.isRead
@@ -81,57 +85,98 @@ function haveRead(bookButt) {
 	})
 }
 
-// Firebase setup
-// Your web app's Firebase configuration
-// var firebaseConfig = {
-// 	apiKey: 'AIzaSyCO5T1pbxrU08QONW0gKPG6JEj4Foh00CU',
-// 	authDomain: 'simple-c89a2.firebaseapp.com',
-// 	databaseURL: 'https://simple-c89a2.firebaseio.com',
-// 	projectId: 'simple-c89a2',
-// 	storageBucket: 'simple-c89a2.appspot.com',
-// 	messagingSenderId: '877479269646',
-// 	appId: '1:877479269646:web:d3d7bec204ed68aa96b35f',
-// }
-// // Initialize Firebase
-// firebase.initializeApp(firebaseConfig)
+// Reload book library
+function reloadBooks(i) {
+	const library = document.getElementsByClassName('library')[0]
+	const libraryBook = document.createElement('div')
+	const bookImg = document.createElement('figure')
+	const figureImg = document.createElement('img')
+	const bookTitle = document.createElement('h4')
+	const bookAuthor = document.createElement('span')
+	const bookPage = document.createElement('span')
+	const buttonSubmit = document.createElement('button')
+	const icon = document.createElement('ion-icon')
+	//Local Storage
+	const localBook = localStorage.getItem(i)
+	const bookObj = JSON.parse(localBook)
+	//Setup
+	// const defaultCover = 'https://source.unsplash.com/RrhhzitYizg/480x720'
+	const defaultCover = 'res/cover.jpeg'
+	libraryBook.className = 'library__book'
+	bookImg.className = 'book__img'
+	figureImg.className = 'figure__img'
+	bookTitle.className = 'book__title'
+	bookAuthor.className = 'book__author'
+	bookPage.className = 'book__page'
+	icon.setAttribute('name', 'book-outline')
+	icon.className = 'isRead'
+	buttonSubmit.className = 'read'
+	bookTitle.innerHTML = bookObj.title
+	bookPage.innerHTML = bookObj.pages
+	bookAuthor.innerHTML = bookObj.author
+	buttonSubmit.setAttribute('onclick', 'haveRead(this)')
 
-// const db = firebase.database()
+	if (Object.keys(bookObj.cover) == 0)
+		figureImg.setAttribute('src', defaultCover)
+	else figureImg.setAttribute('src', bookObj.cover + '/480x720')
+	// bookObj.cover
+	console.log('reloadBooks -> bookObj.cover', bookObj.cover)
+	console.log('bookObj', bookObj)
 
-// butt.addEventListener('click', (e) => {
-// 	console.log('pushed')
-// 	e.preventDefault()
-// 	db.ref('/something/' + test.value).set({
-// 		something: test.value,
-// 	})
-// })
-
-// OPEN ADD BOOK SECTION
-addBookNav.addEventListener('click', () => {
-	if (addBookSection[0].style.display == 'block')
-		addBookSection[0].style.display = 'none'
-	else addBookSection[0].style.display = 'block'
-})
-// todo : to add focus/blur
-
-// ADD FUNCTION TO ADD-BOOK BTN
-addBookBtn.addEventListener('click', () => {
-	localStorage.setItem(
-		localStorage.length + 1,
-		JSON.stringify({
-			title: bookNameAddNewBookSection,
-			author: bookAuthorAddNewBookSection,
-			pages: bookPagesAddNewBookSection,
-			isReadBBB: bookIsReadAddNewBookSection.checked,
-		})
+	console.log(
+		'reloadBooks -> Object.keys(bookObj.cover) === 0',
+		Object.keys(bookObj.cover) == 0
 	)
-	console.log(JSON.parse(localStorage.getItem('test')))
-	console.log(bookIsReadAddNewBookSection.checked)
+	//setup the block
+	buttonSubmit.appendChild(icon)
+	bookImg.appendChild(figureImg)
+	libraryBook.appendChild(bookImg)
+	libraryBook.appendChild(bookTitle)
+	libraryBook.appendChild(bookAuthor)
+	libraryBook.appendChild(bookPage)
+	libraryBook.appendChild(buttonSubmit)
+	library.appendChild(libraryBook)
+}
+
+// FUNCTION TO ADD-BOOK BTN
+addBookBtn.addEventListener('click', () => {
+	// add book window
+	let bookNameAddNewBookSection = document.getElementById('bookName').value
+	let bookAuthorAddNewBookSection = document.getElementById('bookAuthor').value
+	let bookPagesAddNewBookSection = document.getElementById('pageNumber').value
+	const bookIsReadAddNewBookSection = document.getElementById('isReadCheckbox')
+
+	// check is the input is empty
+	if (bookNameAddNewBookSection.length != 0)
+		if (bookAuthorAddNewBookSection != 0)
+			if (bookPagesAddNewBookSection != 0) {
+				localStorage.setItem(
+					localStorage.length + 1,
+					JSON.stringify({
+						title: bookNameAddNewBookSection,
+						author: bookAuthorAddNewBookSection,
+						pages: bookPagesAddNewBookSection,
+						cover: bookCover,
+						isReadBBB: bookIsReadAddNewBookSection.checked,
+					})
+				)
+
+				document.getElementById('bookName').value = ''
+				document.getElementById('bookAuthor').value = ''
+				document.getElementById('pageNumber').value = ''
+				document.getElementById('bookCover').value = ''
+			}
+	// reload books library
+	reloadBooks(localStorage.length)
 })
 
 // Show Local Store
-
 function showLocalStorage() {
 	for (let i = 1; i <= localStorage.length; i++) {
 		console.log(JSON.parse(localStorage.getItem(i)))
 	}
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+	for (let i = 1; i <= localStorage.length; i++) reloadBooks(i)
+})
